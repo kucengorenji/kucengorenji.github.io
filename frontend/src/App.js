6e5f06c8e3714c733e4eb242c1c790ef535b3c38
kucengorenji <mikelrisyad@gmail.com>
2020-12-17 23:10:36 +0700

    upload the emmorce web practice

---
+++ frontend/src/App.js
@@ -0,0 +1,52 @@
+import react from 'react';
+import data from './data';
+
+
+function App() {
+  return (
+    <div className="grid-container">
+        <header className="row">
+            <div>
+                <a className="brand" href="index.html">Amazona</a>
+            </div>
+            <div>
+                <a href="/cart">Cart</a>
+                <a href="/signin">Sign In</a>
+            </div>
+        </header>
+        <main>
+            <div className="row center">
+              {data.products.map((product) => (
+                <div key={product._id} className="card">
+                <a href={`/product/${product._id}`}>
+                    <img 
+                    className="medium" 
+                    src={product.image} 
+                    alt={product.name}></img>
+                </a>
+                <div className="card-body">
+                    <a href={`/product/${product._id}`}>
+                        <h2>{product.name}</h2> 
+                    </a>
+                    <div className="rating">
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                    </div>
+                    <div className="price">{product.price}</div>
+                </div>                      
+            </div>
+              ))}
+
+            </div>            
+        </main>
+        <footer className="row center">
+            All right Reserved
+        </footer>
+    </div>
+  );
+}
+
+export default App;
